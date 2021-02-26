9e6c49e2494bac89f04fe4d5c719f6d0f01184b7
JeremyVk <jeremvk@outlook.fr>
2021-02-26 09:50:33 +0100

    Add scroll Animation

--- app.js
+++ app.js
@@ -12,6 +12,44 @@ hamburger.addEventListener('click', function() {
 })
 close.addEventListener('click', () => {
     mobileNav.classList.toggle('active');
-
 })
+
+// Intersection Observer For scrolling animations
+const ratio = .5;
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+  }
+    // Left container
+const callbackLeft = function(entries, observerLeft) {
+    entries.forEach(function(entry) {
+        if(entry.intersectionRatio > ratio) {
+          
+            entry.target.classList.add('reveal_left_visible');
+            observerLeft.unobserve(entry.target);
+        }
+    })
+}
+
+  let observerLeft = new IntersectionObserver(callbackLeft, options);
+  document.querySelectorAll('.reveal_left').forEach(function(r) {
+    observerLeft.observe(r);
+  })
+
+//   Right Container
+const callbackRight = function(entries, observerRight) {
+    entries.forEach(function(entry) {
+        if(entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal_right_visible');
+            observerRight.unobserve(entry.target);
+        } 
+    })
+}
+
+let observerRight = new IntersectionObserver(callbackRight, options);
+  document.querySelectorAll('.reveal_right').forEach(function(r) {
+    observerRight.observe(r);
+  })
+
 
